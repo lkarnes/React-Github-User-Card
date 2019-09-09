5484f40cc5dbea41459bc15bcab6b66fabe0eb2a
Logan Karnes <logan.karnes98@gmail.com>
2019-09-08 20:31:23 -0700

    MVP done

--- user-cards/src/App.js
+++ user-cards/src/App.js
@@ -5,20 +5,26 @@ import './App.css';
 
 class App extends React.Component {
   state = {
-    user: {}
+    user: {},
+    following: ''
   };
   componentDidMount(){
+    fetch(`https://api.github.com/users/lkarnes/following`)
+    .then(res=> res.json())
+    .then(data=>{this.setState({following: data })})
+    .catch(err=> {console.log(err)})
     fetch(`https://api.github.com/users/lkarnes`)
     .then(res=> res.json())
-    .then(data=> {this.setState({user: data})
-    console.log(data)
-  })
+    .then(data=> {this.setState({user: data})})
     .catch(err=>console.log(err))
   }
   render() {
+    if(!this.state.user){
+      return(<h1>loading..</h1>)
+    }
     return (
     <div className="App">
-      <UserCard data={this.state.user} />
+      <UserCard data={this.state.user} follow={this.state.following} />
     </div>
   );
   }
--- user-cards/src/UserCard.js
+++ user-cards/src/UserCard.js
@@ -1,14 +1,33 @@
 import React from 'react';
 
-function UserCard({data}) {
+function UserCard({data, follow}) {
+    console.log(follow)
+    if(!follow){
+        return(<p>loading...</p>)
+    }
         return(
-            <div>
-                <h1>User Card</h1>
-                <h2>{data.login}</h2>
-                <img src={data.avatar_url}/>
-                <p>{data.name}</p>
-                <p>Located: {data.location}</p>
-                <p>Public Repos: {data.public_repos}</p>
+            <div className='user-card'>
+                <h1 className='card-header'>{data.login}</h1>
+                
+                <div className='user-info'>
+                    <img className='user-img' src={data.avatar_url}/>
+                    <p>{data.name}</p>
+                    <p>Located: {data.location}</p>
+                    <p>Public Repos: {data.public_repos}</p>
+                    <h3 className='f-header'>Followers: {data.followers}</h3>    
+                </div>
+                
+                
+                <div className='following-box'>
+                    <h3 className='f-header'>Following:</h3>
+                    {follow.map(user=>(
+                        <div className='user'>
+                            <img className='following-img' src={user.avatar_url}/>
+                            <p>{user.login}</p>
+                        </div>
+                        
+                    ))}
+                </div>
             </div>
         )
 }
